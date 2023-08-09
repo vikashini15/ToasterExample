e25a5ebcec87c46a2b41e6d964bb02eb2814ab26
VikashiniJagadeesan <vikashini14112000@gmail.com>
2023-08-09 17:26:09 +0530

    Initial Commit

---
+++ Toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,10 @@
+package com.example.toasterlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void s(Context context, String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
